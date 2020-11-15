d8fc949a40f36ce29897708f30e06835e2557585
Sameera Perera <smperera77@gmail.com>
2020-11-15 20:30:06 +0800

    Integreated lombok

--- src/main/java/net/sameera/fbbot/ControllerB.java
+++ src/main/java/net/sameera/fbbot/ControllerB.java
@@ -1,16 +1,16 @@
 package net.sameera.fbbot;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/def")
+@RequiredArgsConstructor
 public class ControllerB {
-    @Autowired
-    private Settings settings;
+    private final Settings settings;
 
     @GetMapping(path = "/hello")
     public String hello() {
--- src/main/java/net/sameera/fbbot/Settings.java
+++ src/main/java/net/sameera/fbbot/Settings.java
@@ -1,34 +1,15 @@
 package net.sameera.fbbot;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "settings")
+@Getter
+@Setter
 public class Settings {
     private String appVersion;
     private String greetMessage;
     private String customMessage;
 
-    public String getAppVersion() {
-        return appVersion;
-    }
-
-    public void setAppVersion(String appVersion) {
-        this.appVersion = appVersion;
-    }
-
-    public String getGreetMessage() {
-        return greetMessage;
-    }
-
-    public void setGreetMessage(String greetMessage) {
-        this.greetMessage = greetMessage;
-    }
-
-    public String getCustomMessage() {
-        return customMessage;
-    }
-
-    public void setCustomMessage(String customMessage) {
-        this.customMessage = customMessage;
-    }
 }
